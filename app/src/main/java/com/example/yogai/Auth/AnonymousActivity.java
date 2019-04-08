8b0d8e3d0a8a45368c1879079e923169f245e7c1
Pranav Sharma <ps20maths@gmail.com>
2019-04-08 06:33:39 +0530

    Initial commit(Project is not in working condition)

---
+++ app/src/main/java/com/example/yogai/Auth/AnonymousActivity.java
@@ -0,0 +1,15 @@
+package com.example.yogai.Auth;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.yogai.R;
+
+public class AnonymousActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_anonymous);
+    }
+}
